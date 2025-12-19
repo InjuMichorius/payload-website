4aad927fecc7adfc447b372a146b3c7d48518d1d
Inju Michorius <i.michorius@am-impact.nl>
2025-12-19 21:53:32 +0100

    Change default texts and put new favicon

--- src/utilities/mergeOpenGraph.ts
+++ src/utilities/mergeOpenGraph.ts
@@ -3,14 +3,14 @@ import { getServerSideURL } from './getURL'
 
 const defaultOpenGraph: Metadata['openGraph'] = {
   type: 'website',
-  description: 'An open-source website built with Payload and Next.js.',
+  description: 'Ward’s Garage: Van oude roestbak tot motormonster',
   images: [
     {
       url: `${getServerSideURL()}/website-template-OG.webp`,
     },
   ],
-  siteName: 'Payload Website Template',
-  title: 'Payload Website Template',
+  siteName: 'Brakes & chains',
+  title: 'Brakes & chains',
 }
 
 export const mergeOpenGraph = (og?: Metadata['openGraph']): Metadata['openGraph'] => {
